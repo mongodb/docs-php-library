baa60ec18c6377b56083613b14cbcfac5387fc44
norareidy <nora.reidy@mongodb.com>
2024-08-27 15:17:40 -0400

    DOCSP-41988: Aggregation

---
+++ source/includes/aggregation.php
@@ -0,0 +1,42 @@
+<?php
+require 'vendor/autoload.php';
+
+$uri = getenv('MONGODB_URI') ?: throw new RuntimeException('Set the MONGODB_URI variable to your Atlas URI that connects to the sample dataset');
+$client = new MongoDB\Client($uri);
+
+$collection = $client->sample_restaurants->restaurants;
+
+// Retrieves documents with a cuisine value of "Bakery", groups them by "borough", and
+// counts each borough's matching documents
+// start-match-group
+$pipeline = [
+    ['$match' => ['cuisine' => 'Bakery']],
+    ['$group' => ['_id' => '$borough', 'count' => ['$sum' => 1]]]
+];
+
+$cursor = $collection->aggregate($pipeline);
+
+foreach ($cursor as $doc) {
+    echo json_encode($doc) . PHP_EOL;
+}
+// end-match-group
+
+// Performs the same aggregation operation as above but asks MongoDB to explain it
+// start-explain
+$pipeline = [
+    ['$match' => ['cuisine' => 'Bakery']],
+    ['$group' => ['_id' => '$borough', 'count' => ['$sum' => 1]]]
+];
+
+$command = [
+    'explain' => [
+        'aggregate' => 'restaurants',
+        'pipeline' => $pipeline,
+        'cursor' => new stdClass()
+    ]
+];
+
+$result = $db->command($command)->toArray();
+echo json_encode($result[0]) . PHP_EOL;
+// end-explain
+
