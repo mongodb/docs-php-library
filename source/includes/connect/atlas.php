e19330cd26dbf2b1a0485ad2a266293c929781a1
Mike Woofter <108414937+mongoKart@users.noreply.github.com>
2024-08-28 11:39:08 -0500

    feedback

--- source/includes/connect/atlas.php
+++ source/includes/connect/atlas.php
@@ -1,23 +1,19 @@
 <?php
 
-try {
-    // Replace the placeholder with your Atlas connection string
-    $uri = "<connection string>";
+// Replace the placeholder with your Atlas connection string
+$uri = "<connection string>";
 
-    // Create a MongoDB client with server API options
-    $client = new MongoDB\Client($uri, [], [
-        'serverApi' => new MongoDB\Driver\ServerApi('1')
-    ]);
+// Create a MongoDB client with server API options
+$client = new MongoDB\Client($uri, [], [
+    'serverApi' => new MongoDB\Driver\ServerApi('1')
+]);
 
-    // Ping the server to verify that the connection works
-    $admin = $client->admin;
-    $command = new MongoDB\Driver\Command(['ping' => 1]);
-    $result = $admin->command($command)->toArray();
+// Ping the server to verify that the connection works
+$admin = $client->admin;
+$command = new MongoDB\Driver\Command(['ping' => 1]);
+$result = $admin->command($command)->toArray();
 
-    echo json_encode($result), "\n";
-    echo "Pinged your deployment. You successfully connected to MongoDB!\n";
-} catch (Exception $e) {
-    echo "An exception occurred: ", $e->getMessage(), "\n";
-    exit(EXIT_FAILURE);
-}
+echo json_encode($result), "\n";
+echo "Pinged your deployment. You successfully connected to MongoDB!\n";
+
 ?>
