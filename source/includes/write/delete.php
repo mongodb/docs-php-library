4470125fb05424e337d35c4a179348154df1c8a6
norareidy <nora.reidy@mongodb.com>
2024-09-06 10:07:19 -0400

    DOCSP-41970: Delete documents

---
+++ source/includes/write/delete.php
@@ -0,0 +1,35 @@
+<?php
+require 'vendor/autoload.php'; // Ensure you have MongoDB PHP library installed
+
+$uri = getenv('MONGODB_URI') ?: throw new RuntimeException('Set the MONGODB_URI variable to your Atlas URI that connects to the sample dataset');
+$client = new MongoDB\Client($uri);
+
+// start-db-coll
+$collection = $client->sample_restaurants->restaurants;
+// end-db-coll
+
+// Deletes a document that has a "name" value of "Ready Penny Inn"
+// start-delete-one
+$result = $collection->deleteOne(['name' => 'Ready Penny Inn']);
+// end-delete-one
+
+// Deletes documents that have a "borough" value of "Brooklyn"
+// start-delete-many
+$result = $collection->deleteMany(['borough' => 'Brooklyn']);
+// end-delete-many
+
+// Deletes matching documents and attaches a comment to the operation
+// start-delete-options
+$result = $collection->deleteMany(
+    ['name' => new MongoDB\BSON\Regex('Mongo', '')],
+    ['comment' => 'Deleting Mongo restaurants'],
+);
+// end-delete-options
+
+// Deletes and prints the number of documents that have a "cuisine" value of "Greek"
+// start-delete-count
+$result = $collection->deleteMany(['cuisine' => 'Greek']);
+echo $result->getDeletedCount() . PHP_EOL;
+// end-delete-count
+
+?>
