13e1c13ad1e8869150f7a4ead62d17d1c00d6383
rustagir <rea.rustagi@mongodb.com>
2024-09-09 13:21:59 -0400

    JT tech review 1

--- source/includes/write/run-command.php
+++ source/includes/write/run-command.php
@@ -4,16 +4,14 @@ require __DIR__ . '/vendor/autoload.php';
 
 use MongoDB\Client;
 
-$client = new Client('<connection string>');
+$uri = getenv('MONGODB_URI') ?: throw new RuntimeException('Set the MONGODB_URI variable to your Atlas URI that connects to the sample dataset');
+$client = new MongoDB\Client($uri);
 
 // start-runcommand
-$database = $client->plants;
+$database = $client->accounts;
+$command = ['dbStats' => 1];
 
-$countCommand = ['count' => 'flowers'];
-$explainCommand = ['explain' => $countCommand, 'verbosity' => 'queryPlanner'];
-
-$result = $database->command($explainCommand);
+$result = $database->command($command);
 // end-runcommand
 
-var_dump($result->toArray());
-
+var_dump(json_encode($result->toArray()));
