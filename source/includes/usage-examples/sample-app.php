e227a7fdb3b62954dc39819fb5a0c0ef8096606a
rustagir <rea.rustagi@mongodb.com>
2024-09-10 10:29:42 -0400

    JM tech review 1

--- source/includes/usage-examples/sample-app.php
+++ source/includes/usage-examples/sample-app.php
@@ -2,10 +2,10 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-use MongoDB\Client;
+$uri = getenv('MONGODB_URI') ?: throw new RuntimeException('Set the MONGODB_URI variable to your Atlas URI that connects to the sample dataset');
+$client = new MongoDB\Client($uri);
 
-$client = new Client('<connection string>');
-$collection = $client-><database>-><collection>;
+$collection = $client->selectCollection('<database>', '<collection>');
 
 // Start example code here
 
