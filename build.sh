070aac5bc02591a24b9e7a62f8f2e02f117134dc
Brandon Ly <brandonly@lostcoding.com>
2024-09-10 12:17:20 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 if [ -f build-site.sh ]; then
   rm build-site.sh
 fi 
-
+test
 
 curl https://raw.githubusercontent.com/mongodb/docs-worker-pool/netlify-poc/scripts/build-site.sh -o build-site.sh 
 sh build-site.sh
