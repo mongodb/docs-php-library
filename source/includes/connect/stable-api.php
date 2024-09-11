dd323f707e90d071b06af86bb2cb1bb4f7509589
Mike Woofter <108414937+mongoKart@users.noreply.github.com>
2024-09-11 12:14:06 -0500

    feedback

--- source/includes/connect/stable-api.php
+++ source/includes/connect/stable-api.php
@@ -1,23 +1,15 @@
 // start-specify-v1
-<?php
-
 $uri = "mongodb://<hostname>:<port>";
 
-$driverOptions = [
-    'serverApi' => new MongoDB\Driver\ServerApi('1')
-];
+$driverOptions = ['serverApi' => new MongoDB\Driver\ServerApi('1')];
 
 $client = new MongoDB\Client($uri, [], $driverOptions);
 // end-specify-v1
 
 // start-stable-api-options
-<?php
-
 $uri = "mongodb://<hostname>:<port>";
 
-$driverOptions = [
-    'serverApi' => new MongoDB\Driver\ServerApi('1', true, true)
-];
+$driverOptions = ['serverApi' => new MongoDB\Driver\ServerApi('1', strict: true, deprecationErrors: true)];
 
 $client = new MongoDB\Client($uri, [], $driverOptions);
 // end-stable-api-options
