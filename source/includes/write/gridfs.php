8ed297575b51af4d8a91ef5017d2b652a4e64702
norareidy <nora.reidy@mongodb.com>
2024-09-11 16:18:04 -0400

    fix

--- source/includes/write/gridfs.php
+++ source/includes/write/gridfs.php
@@ -4,6 +4,7 @@ require 'vendor/autoload.php'; // include Composer's autoloader
 use MongoDB\Client;
 use MongoDB\BSON\ObjectID;
 use MongoDB\GridFS\Bucket;
+use MongoDB\Driver\Manager;
 
 $uri = getenv('MONGODB_URI') ?: throw new RuntimeException('Set the MONGODB_URI variable to your Atlas URI that connects to the sample dataset');
 $client = new MongoDB\Client($uri);
