4fd14519f1ece0ab2a15cb15ce1ed3e6e80f412a
rustagir <rea.rustagi@mongodb.com>
2024-09-12 16:49:57 -0400

    DOCSP-41986: multikey indexes

--- source/includes/indexes/indexes.php
+++ source/includes/indexes/indexes.php
@@ -30,3 +30,14 @@ $indexName = $collection->createIndex(['title' => 1]);
 $document = $collection->findOne(['title' => 'Sweethearts']);
 echo json_encode($document) , PHP_EOL;
 // end-index-single-query
+
+// start-multikey
+$indexName = $collection->createIndex(['cast' => 1]);
+// end-multikey
+
+// start-index-array-query
+$document = $collection->findOne(
+    ['cast' => ['$in' => ['Aamir Khan', 'Kajol']]]
+);
+echo json_encode($document) , PHP_EOL;
+// end-index-array-query
