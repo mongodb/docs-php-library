c59051410ce8d34847df3bb005f35b83f60d353d
norareidy <nora.reidy@mongodb.com>
2024-09-13 11:47:44 -0400

    JM most feedback

--- source/includes/write/gridfs.php
+++ source/includes/write/gridfs.php
@@ -9,64 +9,63 @@ use MongoDB\Driver\Manager;
 $uri = getenv('MONGODB_URI') ?: throw new RuntimeException('Set the MONGODB_URI variable to your Atlas URI that connects to the sample dataset');
 $client = new MongoDB\Client($uri);
 
-// Creates a GridFS bucket or references an existing one
-// start create bucket
-$db = $client->db;
-$bucket = $db->selectGridFSBucket();
-// end create bucket
+// start-to-json
+function toJSON(object $document): string
+{
+    return MongoDB\BSON\Document::fromPHP($document)->toRelaxedExtendedJSON();
+}
+// end-to-json
 
-// Constructs a GridFS bucket
-// start create bucket explicit
-$manager = new Manager('<connection string>');
-$bucket = new Bucket($manager, 'db');
-// end create bucket explicit
+// Creates a GridFS bucket or references an existing one
+// start-create-bucket
+$bucket = $client->db->selectGridFSBucket();
+// end-create-bucket
 
 // Creates or references a GridFS bucket with a custom name
-// start create custom bucket
+// start-create-custom-bucket
 $custom_bucket = $client->db->selectGridFSBucket(
     ['bucketName' => 'myCustomBucket']
 );
-// end create custom bucket
+// end-create-custom-bucket
 
 // Uploads a file called "my_file" to the GridFS bucket and writes data to it
-// start upload files
+// start-upload-files
 $stream = $bucket->openUploadStream('my_file', [
-    'chunkSizeBytes' => 1048576,
     'metadata' => ['contentType' => 'text/plain']
 ]);
 fwrite($stream, 'Data to store');
 fclose($stream);
-// end upload files
+// end-upload-files
 
 // Prints information about each file in the bucket
-// start retrieve file info
+// start-retrieve-file-info
 $files = $bucket->find();
 foreach ($files as $file_doc) {
-    echo json_encode($file_doc) , PHP_EOL;
+    echo toJSON($file_doc), PHP_EOL;
 }
-// end retrieve file info
+// end-retrieve-file-info
 
 // Downloads the "my_file" file from the GridFS bucket and prints its contents
-// start download files name
+// start-download-files-name
 $stream = $bucket->openDownloadStreamByName('my_file');
 $contents = stream_get_contents($stream);
-echo json_encode($contents) , PHP_EOL;
+echo $contents, PHP_EOL;
 fclose($stream);
-// end download files name
+// end-download-files-name
 
 // Downloads a file from the GridFS bucket by referencing its ObjectID value
-// start download files id
+// start-download-files-id
 $stream = $bucket->openDownloadStream(new ObjectID('66e0a5487c880f844c0a32b1'));
 $contents = stream_get_contents($stream);
 fclose($stream);
-// end download files id
+// end-download-files-id
 
 // Renames a file from the GridFS bucket with the specified ObjectID
-// start rename files
+// start-rename-files
 $bucket->rename(new ObjectID('66e0a5487c880f844c0a32b1'), 'new_file_name');
-// end rename files
+// end-rename-files
 
 // Deletes a file from the GridFS bucket with the specified ObjectID
-// start delete files
+// start-delete-files
 $bucket->delete(new ObjectID('66e0a5487c880f844c0a32b1'));
-// end delete files
+// end-delete-files
