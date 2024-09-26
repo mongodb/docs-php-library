2c12b6b61e37f797d8f383b256b8fc13eaaacc8b
rustagir <rea.rustagi@mongodb.com>
2024-09-26 15:33:56 -0400

    JM tech review 1

--- source/includes/write/transaction.php
+++ source/includes/write/transaction.php
@@ -9,37 +9,41 @@ $receipts = $client->bank->receipts;
 $checking = $client->bank->checking_accounts;
 $saving = $client->bank->saving_accounts;
 
-$accountId = '5678';
-$transferAmount = 1000.00;
+$accountId = "5678";
+$transferAmount = 1000.0;
 
-$callback = function (MongoDB\Driver\Session $session) 
-    use ($checking, $saving, $receipts, $accountId, $transferAmount): void {
-
+$callback = function (MongoDB\Driver\Session $session) use (
+    $checking,
+    $saving,
+    $receipts,
+    $accountId,
+    $transferAmount
+): void {
     $checking->updateOne(
-           ['account_id' => $accountId],
-           ['$inc' => ['balance' => -$transferAmount]],
-           ['session' => $session]
+        ["account_id" => $accountId],
+        ['$inc' => ["balance" => -$transferAmount]],
+        ["session" => $session]
     );
-    
+
     $saving->updateOne(
-        ['account_id' => $accountId],
-        ['$inc' => ['balance' => $transferAmount]],
-        ['session' => $session]
+        ["account_id" => $accountId],
+        ['$inc' => ["balance" => $transferAmount]],
+        ["session" => $session]
     );
 
-    $summary = sprintf(
-        "SAVINGS +%u CHECKING -%u", $transferAmount, $transferAmount
+    $summary = sprintf('SAVINGS +%1$u CHECKING -%1$u', $transferAmount);
+
+    $receipts->insertOne(
+        [
+            "account_id" => $accountId,
+            "summary" => $summary,
+            "timestamp" => new MongoDB\BSON\UTCDateTime(),
+        ],
+        ["session" => $session]
     );
 
-    $receipts->insertOne([
-        'account_id' => $accountId,
-        'summary' => $summary,
-        'timestamp' => new MongoDB\BSON\UTCDateTime(),
-    ],
-    ['session' => $session]);
-
-    echo 'Successfully performed transaction!' , PHP_EOL;
-    echo 'Summary: ', $summary, PHP_EOL;
+    echo "Successfully performed transaction!", PHP_EOL;
+    echo "Summary: ", $summary, PHP_EOL;
 };
 // end-callback
 
@@ -48,7 +52,7 @@ $session = $client->startSession();
 
 try {
     MongoDB\with_transaction($session, $callback);
-} catch (Exception $e) {
-    echo 'Caught exception: ', $e->getMessage(), "\n";
+} catch (MongoDB\Driver\Exception\RuntimeException $e) {
+    echo "Caught exception: ", $e->getMessage(), PHP_EOL;
 }
 // end-txn
