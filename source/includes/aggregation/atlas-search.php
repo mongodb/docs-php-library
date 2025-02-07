9d4aae25c942deb2445cb44b436e336c7d3796f4
rustagir <rea.rustagi@mongodb.com>
2025-02-07 16:42:58 -0500

    JT tech review 1

--- source/includes/aggregation/atlas-search.php
+++ source/includes/aggregation/atlas-search.php
@@ -1,5 +1,11 @@
 <?php
 
+// start-imports
+use MongoDB\Builder\Pipeline;
+use MongoDB\Builder\Search;
+use MongoDB\Builder\Stage;
+// end-imports
+
 require 'vendor/autoload.php';
 
 $uri = getenv('MONGODB_URI') ?: throw new RuntimeException('Set the MONGODB_URI variable to your Atlas URI that connects to the sample dataset');
@@ -7,54 +13,134 @@ $client = new MongoDB\Client($uri);
 
 $collection = $client->sample_restaurants->restaurants;
 
-// start-compound-search-query
-$pipeline = new Pipeline(
-    Stage::search(
-        Search::compound(
-            must: [
-                Search::text(
-                    query: 'kitchen',
-                    path: 'name',
-                ),
-            ],
-            should: [
-                Search::text(
-                    query: 'american',
-                    path: 'cuisine',
-                ),
-            ],
-            filter: [
-                Search::text(
-                    query: 'Queens',
-                    path: 'borough',
-                ),
-            ],
-        ),
-    ),
+define('WAIT_TIMEOUT_SEC', 300);
+
+echo "\nCreating the Atlas Search index.\n";
+$collection->createSearchIndex(
+	['mappings' => ['dynamic' => true]],
 );
 
-$cursor = $collection->aggregate($pipeline);
+// Waits for the index to be queryable
+wait(function () use ($collection) {
+	echo '.';
+	foreach ($collection->listSearchIndexes() as $index) {
+		if ($index->name === 'default') {
+			return $index->queryable;
+		}
+	}
+
+	return false;
+});
+
+echo "\n";
+
+// start-compound-search-query
+$pipeline = new Pipeline(
+	Stage::search(
+		Search::compound(
+			must: [
+				Search::text(
+					query: 'kitchen',
+					path: 'name',
+				),
+			],
+			should: [
+				Search::text(
+					query: 'american',
+					path: 'cuisine',
+				),
+			],
+			filter: [
+				Search::text(
+					query: 'Queens',
+					path: 'borough',
+				),
+			],
+		),
+	),
+	Stage::project(
+		borough: 1,
+		cuisine: 1,
+		name: 1
+	),
+	Stage::limit(3)
+);
+
+$cursor = $collection->aggregate(iterator_to_array($pipeline));
 
 foreach ($cursor as $doc) {
-    echo json_encode($doc), PHP_EOL;
+	echo json_encode($doc), PHP_EOL;
 }
 // end-compound-search-query
 
-// start-autocomplete-search-query
-$pipeline = new Pipeline(
-    Stage::search(
-        Search::autocomplete(
-            query: 'Lucy',
-            path: 'name',
-        ),
-    ),
-    Stage::limit(10),
-    Stage::project(_id: 0, title: 1),
+echo "\nUpdating the Atlas Search index for autocomplete.\n";
+$collection->updateSearchIndex(
+	'default',
+	['mappings' => [
+		"dynamic" => false,
+		"fields" => [
+			"name" => [
+				["type" => "stringFacet"],
+				["type" => "string"],
+				[
+					"foldDiacritics" => false,
+					"maxGrams" => 7,
+					"minGrams" => 3,
+					"tokenization" => "edgeGram",
+					"type" => "autocomplete"
+				],
+			]
+		]
+	]]
 );
 
-$cursor = $collection->aggregate($pipeline);
+// Waits for the index to be updated.
+wait(function () use ($collection) {
+	echo '.';
+	foreach ($collection->listSearchIndexes() as $index) {
+		if ($index->name === 'default') {
+			return $index->latestDefinition->mappings->fields->name[2]['type'] === 'autocomplete'
+				&& $index->status === 'READY';
+		}
+	}
+
+	return false;
+});
+
+echo "\n";
+
+// start-autocomplete-search-query
+$pipeline = new Pipeline(
+	Stage::search(
+		Search::autocomplete(
+			query: 'Lucy',
+			path: 'name',
+		),
+	),
+	Stage::limit(3),
+	Stage::project(_id: 0, name: 1),
+);
+
+$cursor = $collection->aggregate(iterator_to_array($pipeline));
 
 foreach ($cursor as $doc) {
-    echo json_encode($doc), PHP_EOL;
+	echo json_encode($doc), PHP_EOL;
 }
 // end-autocomplete-search-query
+
+/**
+ * This function waits until the callback returns true or the timeout is reached.
+ */
+function wait(Closure $callback): void
+{
+	$timeout = hrtime()[0] + WAIT_TIMEOUT_SEC;
+	while (hrtime()[0] < $timeout) {
+		if ($callback()) {
+			return;
+		}
+
+		sleep(5);
+	}
+
+	throw new RuntimeException('Time out');
+}
