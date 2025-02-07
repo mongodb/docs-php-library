491f26ad47f0e12062efa5061eb5e5a67d4bb0a1
rustagir <rea.rustagi@mongodb.com>
2025-02-07 16:44:24 -0500

    indentation fix

--- source/includes/aggregation/atlas-search.php
+++ source/includes/aggregation/atlas-search.php
@@ -17,114 +17,114 @@ define('WAIT_TIMEOUT_SEC', 300);
 
 echo "\nCreating the Atlas Search index.\n";
 $collection->createSearchIndex(
-	['mappings' => ['dynamic' => true]],
+    ['mappings' => ['dynamic' => true]],
 );
 
 // Waits for the index to be queryable
 wait(function () use ($collection) {
-	echo '.';
-	foreach ($collection->listSearchIndexes() as $index) {
-		if ($index->name === 'default') {
-			return $index->queryable;
-		}
-	}
+    echo '.';
+    foreach ($collection->listSearchIndexes() as $index) {
+        if ($index->name === 'default') {
+            return $index->queryable;
+        }
+    }
 
-	return false;
+    return false;
 });
 
 echo "\n";
 
 // start-compound-search-query
 $pipeline = new Pipeline(
-	Stage::search(
-		Search::compound(
-			must: [
-				Search::text(
-					query: 'kitchen',
-					path: 'name',
-				),
-			],
-			should: [
-				Search::text(
-					query: 'american',
-					path: 'cuisine',
-				),
-			],
-			filter: [
-				Search::text(
-					query: 'Queens',
-					path: 'borough',
-				),
-			],
-		),
-	),
-	Stage::project(
-		borough: 1,
-		cuisine: 1,
-		name: 1
-	),
-	Stage::limit(3)
+    Stage::search(
+        Search::compound(
+            must: [
+                Search::text(
+                    query: 'kitchen',
+                    path: 'name',
+                ),
+            ],
+            should: [
+                Search::text(
+                    query: 'american',
+                    path: 'cuisine',
+                ),
+            ],
+            filter: [
+                Search::text(
+                    query: 'Queens',
+                    path: 'borough',
+                ),
+            ],
+        ),
+    ),
+    Stage::project(
+        borough: 1,
+        cuisine: 1,
+        name: 1
+    ),
+    Stage::limit(3)
 );
 
 $cursor = $collection->aggregate(iterator_to_array($pipeline));
 
 foreach ($cursor as $doc) {
-	echo json_encode($doc), PHP_EOL;
+    echo json_encode($doc), PHP_EOL;
 }
 // end-compound-search-query
 
 echo "\nUpdating the Atlas Search index for autocomplete.\n";
 $collection->updateSearchIndex(
-	'default',
-	['mappings' => [
-		"dynamic" => false,
-		"fields" => [
-			"name" => [
-				["type" => "stringFacet"],
-				["type" => "string"],
-				[
-					"foldDiacritics" => false,
-					"maxGrams" => 7,
-					"minGrams" => 3,
-					"tokenization" => "edgeGram",
-					"type" => "autocomplete"
-				],
-			]
-		]
-	]]
+    'default',
+    ['mappings' => [
+        "dynamic" => false,
+        "fields" => [
+            "name" => [
+                ["type" => "stringFacet"],
+                ["type" => "string"],
+                [
+                    "foldDiacritics" => false,
+                    "maxGrams" => 7,
+                    "minGrams" => 3,
+                    "tokenization" => "edgeGram",
+                    "type" => "autocomplete"
+                ],
+            ]
+        ]
+    ]]
 );
 
 // Waits for the index to be updated.
 wait(function () use ($collection) {
-	echo '.';
-	foreach ($collection->listSearchIndexes() as $index) {
-		if ($index->name === 'default') {
-			return $index->latestDefinition->mappings->fields->name[2]['type'] === 'autocomplete'
-				&& $index->status === 'READY';
-		}
-	}
+    echo '.';
+    foreach ($collection->listSearchIndexes() as $index) {
+        if ($index->name === 'default') {
+            return $index->latestDefinition->mappings->fields->name[2]['type'] === 'autocomplete'
+                && $index->status === 'READY';
+        }
+    }
 
-	return false;
+    return false;
 });
 
 echo "\n";
 
 // start-autocomplete-search-query
 $pipeline = new Pipeline(
-	Stage::search(
-		Search::autocomplete(
-			query: 'Lucy',
-			path: 'name',
-		),
-	),
-	Stage::limit(3),
-	Stage::project(_id: 0, name: 1),
+    Stage::search(
+        Search::autocomplete(
+            query: 'Lucy',
+            path: 'name',
+        ),
+    ),
+    Stage::limit(3),
+    Stage::project(_id: 0, name: 1),
 );
 
 $cursor = $collection->aggregate(iterator_to_array($pipeline));
 
 foreach ($cursor as $doc) {
-	echo json_encode($doc), PHP_EOL;
+    echo json_encode($doc), PHP_EOL;
 }
 // end-autocomplete-search-query
 
@@ -133,14 +133,14 @@ foreach ($cursor as $doc) {
  */
 function wait(Closure $callback): void
 {
-	$timeout = hrtime()[0] + WAIT_TIMEOUT_SEC;
-	while (hrtime()[0] < $timeout) {
-		if ($callback()) {
-			return;
-		}
+    $timeout = hrtime()[0] + WAIT_TIMEOUT_SEC;
+    while (hrtime()[0] < $timeout) {
+        if ($callback()) {
+            return;
+        }
 
-		sleep(5);
-	}
+        sleep(5);
+    }
 
-	throw new RuntimeException('Time out');
+    throw new RuntimeException('Time out');
 }
