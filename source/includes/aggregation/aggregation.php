99d6179f6323a68ec499bf989a1fbaf823f36c54
rustagir <rea.rustagi@mongodb.com>
2025-02-10 12:56:58 -0500

    DOCSP-43919: aggregation builder

--- source/includes/aggregation/aggregation.php
+++ source/includes/aggregation/aggregation.php
@@ -1,4 +1,13 @@
 <?php
+
+use MongoDB\BSON\UTCDateTime;
+use MongoDB\Builder\Accumulator;
+use MongoDB\Builder\Expression;
+use MongoDB\Builder\Pipeline;
+use MongoDB\Builder\Query;
+use MongoDB\Builder\Stage;
+use MongoDB\Builder\Type\Sort;
+
 require 'vendor/autoload.php';
 
 $uri = getenv('MONGODB_URI') ?: throw new RuntimeException('Set the MONGODB_URI variable to your Atlas URI that connects to the sample dataset');
@@ -8,33 +17,105 @@ $collection = $client->sample_restaurants->restaurants;
 
 // Retrieves documents with a cuisine value of "Bakery", groups them by "borough", and
 // counts each borough's matching documents
-// start-match-group
+// start-array-match-group
 $pipeline = [
-    ['$match' => ['cuisine' => 'Bakery']],
-    ['$group' => ['_id' => '$borough', 'count' => ['$sum' => 1]]],
+	['$match' => ['cuisine' => 'Bakery']],
+	['$group' => ['_id' => '$borough', 'count' => ['$sum' => 1]]],
 ];
 
 $cursor = $collection->aggregate($pipeline);
 
 foreach ($cursor as $doc) {
-    echo json_encode($doc), PHP_EOL;
+	echo json_encode($doc), PHP_EOL;
 }
-// end-match-group
+// end-array-match-group
 
 // Performs the same aggregation operation as above but asks MongoDB to explain it
-// start-explain
+// start-array-explain
 $pipeline = [
-    ['$match' => ['cuisine' => 'Bakery']],
-    ['$group' => ['_id' => '$borough', 'count' => ['$sum' => 1]]],
+	['$match' => ['cuisine' => 'Bakery']],
+	['$group' => ['_id' => '$borough', 'count' => ['$sum' => 1]]],
 ];
 
 $aggregate = new MongoDB\Operation\Aggregate(
-    $collection->getDatabaseName(),
-    $collection->getCollectionName(),
-    $pipeline
+	$collection->getDatabaseName(),
+	$collection->getCollectionName(),
+	$pipeline
 );
 
 $result = $collection->explain($aggregate);
 echo json_encode($result), PHP_EOL;
-// end-explain
+// end-array-explain
 
+// start-builder-match-group
+$pipeline = new Pipeline(
+	Stage::match(
+		date: [
+			Query::gte(new UTCDateTime(new DateTimeImmutable('2014-01-01'))),
+			Query::lt(new UTCDateTime(new DateTimeImmutable('2015-01-01'))),
+		],
+	),
+	Stage::group(
+		_id: Expression::dateToString(Expression::dateFieldPath('date'), '%Y-%m-%d'),
+		totalSaleAmount: Accumulator::sum(
+			Expression::multiply(
+				Expression::numberFieldPath('price'),
+				Expression::numberFieldPath('quantity'),
+			),
+		),
+		averageQuantity: Accumulator::avg(
+			Expression::numberFieldPath('quantity'),
+		),
+		count: Accumulator::sum(1),
+	),
+	Stage::sort(
+		totalSaleAmount: Sort::Desc,
+	),
+);
+
+$cursor = $collection->aggregate(iterator_to_array($pipeline));
+
+foreach ($cursor as $doc) {
+	echo json_encode($doc), PHP_EOL;
+}
+// end-builder-match-group
+
+// start-builder-unwind
+$pipeline = new Pipeline(
+	Stage::unwind(Expression::arrayFieldPath('items')),
+	Stage::unwind(Expression::arrayFieldPath('items.tags')),
+	Stage::group(
+		_id: Expression::fieldPath('items.tags'),
+		totalSalesAmount: Accumulator::sum(
+			Expression::multiply(
+				Expression::numberFieldPath('items.price'),
+				Expression::numberFieldPath('items.quantity'),
+			),
+		),
+	),
+);
+
+$cursor = $collection->aggregate(iterator_to_array($pipeline));
+
+foreach ($cursor as $doc) {
+	echo json_encode($doc), PHP_EOL;
+}
+// end-builder-unwind
+
+// start-builder-lookup
+$pipeline = new Pipeline(
+	Stage::lookup(
+		from: 'inventory',
+		localField: 'item',
+		foreignField: 'sku',
+		as: 'inventory_docs',
+	),
+);
+
+/* Perform the aggregation on the orders collection */
+$cursor = $collection->aggregate(iterator_to_array($pipeline));
+
+foreach ($cursor as $doc) {
+	echo json_encode($doc), PHP_EOL;
+}
+// end-builder-lookup
