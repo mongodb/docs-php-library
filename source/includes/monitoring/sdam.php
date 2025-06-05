902ea35c5d7d67dd37856856103f672acc8464d2
Andreas Braun <andreas.braun@mongodb.com>
2025-06-05 09:58:56 +0200

    Fix multiline function declarations

--- source/includes/monitoring/sdam.php
+++ source/includes/monitoring/sdam.php
@@ -14,7 +14,8 @@ class MySubscriber implements MongoDB\Driver\Monitoring\SDAMSubscriber
         $this->stream = $stream;
     }
 
-    public function serverOpening(MongoDB\Driver\Monitoring\ServerOpeningEvent $event): void {
+    public function serverOpening(MongoDB\Driver\Monitoring\ServerOpeningEvent $event): void
+    {
         fprintf(
             $this->stream,
             'Server opening on %s:%s\n',
@@ -23,14 +24,37 @@ class MySubscriber implements MongoDB\Driver\Monitoring\SDAMSubscriber
         );
     }
 
-    public function serverClosed(MongoDB\Driver\Monitoring\ServerClosedEvent $event): void {}
-    public function serverChanged(MongoDB\Driver\Monitoring\ServerChangedEvent $event): void {}
-    public function serverHeartbeatFailed(MongoDB\Driver\Monitoring\ServerHeartbeatFailedEvent $event): void {}
-    public function serverHeartbeatStarted(MongoDB\Driver\Monitoring\ServerHeartbeatStartedEvent $event): void {}
-    public function serverHeartbeatSucceeded(MongoDB\Driver\Monitoring\ServerHeartbeatSucceededEvent $event): void {}
-    public function topologyChanged(MongoDB\Driver\Monitoring\TopologyChangedEvent $event): void {}
-    public function topologyClosed(MongoDB\Driver\Monitoring\TopologyClosedEvent $event): void {}
-    public function topologyOpening(MongoDB\Driver\Monitoring\TopologyOpeningEvent $event): void {}
+    public function serverClosed(MongoDB\Driver\Monitoring\ServerClosedEvent $event): void
+    {
+    }
+
+    public function serverChanged(MongoDB\Driver\Monitoring\ServerChangedEvent $event): void
+    {
+    }
+
+    public function serverHeartbeatFailed(MongoDB\Driver\Monitoring\ServerHeartbeatFailedEvent $event): void
+    {
+    }
+
+    public function serverHeartbeatStarted(MongoDB\Driver\Monitoring\ServerHeartbeatStartedEvent $event): void
+    {
+    }
+
+    public function serverHeartbeatSucceeded(MongoDB\Driver\Monitoring\ServerHeartbeatSucceededEvent $event): void
+    {
+    }
+
+    public function topologyChanged(MongoDB\Driver\Monitoring\TopologyChangedEvent $event): void
+    {
+    }
+
+    public function topologyClosed(MongoDB\Driver\Monitoring\TopologyClosedEvent $event): void
+    {
+    }
+
+    public function topologyOpening(MongoDB\Driver\Monitoring\TopologyOpeningEvent $event): void
+    {
+    }
 }
 // end-mysubscriber
 
--- source/includes/usage-examples/index-code-examples.php
+++ source/includes/usage-examples/index-code-examples.php
@@ -54,7 +54,7 @@ $collection->updateSearchIndex(
             ],
         ],
     ]],
- );
+);
 // end-search-update
 
 // start-search-delete
