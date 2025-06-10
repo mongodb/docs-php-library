f37d0fe9a30decd2113fa76f55ac9fc8da50db31
norareidy <nora.reidy@mongodb.com>
2025-06-10 11:03:23 -0400

    DOCSP-47063: Logging

---
+++ source/includes/monitoring-logging/logging.php
@@ -0,0 +1,41 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Psr\Log\AbstractLogger;
+use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
+use MongoDB\PsrLogAdapter;
+
+use function MongoDB\add_logger;
+use function MongoDB\remove_logger;
+
+// start-register-logger
+class MyLogger extends AbstractLogger
+{
+    public array $logs = [];
+
+    public function log($level, $message, array $context = []): void
+    {
+        $this->logs[] = [$level, $message, $context['domain']];
+    }
+}
+
+$logger = new MyLogger();
+add_logger($logger);
+
+$uri = "mongodb://a.mongo.cosmos.azure.com:19555/";
+$client = new MongoDB\Client($uri);
+print_r($logger->logs);
+// end-register-logger
+
+// start-write-messages
+PsrLogAdapter::writeLog(PsrLogAdapter::WARN, 'domain1', 'This is a warning message');
+PsrLogAdapter::writeLog(PsrLogAdapter::CRITICAL, 'domain2', 'This is a critical message');
+
+print_r($logger->logs);
+// end-write-messages
+
+// start-remove-logger
+remove_logger($logger);
+// end-remove-logger
